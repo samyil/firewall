ac7a43ef1ebf8d1e0b06e29b0d9e3e9df4f1c2a8
samyil <33155361+samyil@users.noreply.github.com>
2017-10-27 22:21:14 +0300

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -20,7 +20,7 @@ iptables -A INPUT -i lo -j ACCEPT
 #iptables -A INPUT -p tcp --dport 21 -j ACCEPT
 #iptables -A INPUT -p udp --dport 53 -j ACCEPT
 #iptables -A INPUT -p udp --dport 123 -j ACCEPT
-#iptables -A INPUT -p tcp -m multiport --dports 80,443 -j DROP
+#iptables -A INPUT -p tcp -m multiport --dports 80,443 -j ACCEPT
 #iptables -A INPUT -p tcp -m multiport --dports 25,110,143,465,587,993,995 -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p icmp -j ACCEPT
